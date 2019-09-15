72cba020aa9322455d1b1502bb31edd8308ee6eb
Rohuu48 <pisces.rohit99@gmail.com>
2019-09-15 21:34:08 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyAyH3sD8vMutLIDH-vxZfMGUuAPhykriu0",
+  authDomain: "userreg-98698.firebaseapp.com",
+  databaseURL: "https://userreg-98698.firebaseio.com",
+  projectId: "userreg-98698",
+  storageBucket: "userreg-98698.appspot.com",
+  messagingSenderId: "355513339102",
+  appId: "1:355513339102:web:cc292f6df82a753b76864f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
